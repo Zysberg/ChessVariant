8575ec5c541142653493edd8cbb273e21e491383
IchBinZachary <zacharyzysberg1@gmail.com>
2018-07-10 23:12:11 -0400

    added 2nd board

--- Board.js
+++ Board.js
@@ -5,30 +5,31 @@
 function makeBoard() {
     for (var i = 0; i < 10; i++) {
         var row = document.createElement("tr");
+        var row1 = document.createElement("tr");
         for (var j = 0; j < 10; j++) {
-            var cell = createCell(i, j);
+            var cell = createGroundCell(i, j);
+            var cell1 = createSkyCell(i,j);
             row.appendChild(cell);
+            row1.appendChild(cell1);
         }
-        htmlBoard.appendChild(row);
+        groundBoard.appendChild(row);
+        skyBoard.appendChild(row1);
     }
 }
 
 //Color Helper
 function cellColor(cell,i,j){
-	    if ((i + j) % 2 == 0) {
-            cell.setAttribute("style", "background-color: #195919")
-        } else {
-            cell.setAttribute("style", "background-color: #339933")
-        }
+	    if ((i + j) % 2 == 0) {cell.setAttribute("style", "background-color: #195919")} 
+	    else {cell.setAttribute("style", "background-color: #339933")}
 }
 
-//Controls color of cells
-function createCell(i, j) {
+//creates Ground Cell
+function createGroundCell(i, j) {
     var cell = document.createElement("td");
     cell.setAttribute("id", i + " " + j);
     cell.setAttribute("align","center");
-    var cellText = document.createTextNode("");
-    cell.appendChild(cellText);
+    // var cellText = document.createTextNode("");
+    // cell.appendChild(cellText);
     cellColor(cell,i,j);
     cell.addEventListener("mouseout", function() {cellColor(cell,i,j);})
     cell.addEventListener("mouseover", function() {this.setAttribute("style", "background-color:#aaaaaa")});
@@ -36,67 +37,19 @@ function createCell(i, j) {
     return cell;
 }
 
+//Creates SkyCell
+function createSkyCell(i,j){
+	var cell1 = document.createElement("td");
+	cell1.setAttribute("id",i+" "+j);
+	cell1.setAttribute("align","center");
+	var cellText1 = document.createTextNode("");
+	cell1.appendChild(cellText1);
+	cell1.addEventListener("click", function(){this.setAttribute("style","background-color:#777777")});
+	cell1.addEventListener("mouseout",function(){this.style=""})
+	 return cell1;
+}
+
 
 
 //Calls Necessary Functions
 makeBoard();
-document.getElementById('0 0').appendChild(blackAA1);
-
-document.getElementById('1 0').appendChild(blackAA);
-
-
-document.getElementById('0 1').appendChild(whiteAA1);
-
-document.getElementById('1 1').appendChild(whiteAA);
-//
-document.getElementById('0 2').appendChild(blackS);
-
-document.getElementById('1 2').appendChild(blackS1);
-
-document.getElementById('0 3').appendChild(whiteS);
-
-document.getElementById('1 3').appendChild(whiteS1);
-//
-document.getElementById('0 4').appendChild(blackHV);
-
-document.getElementById('1 4').appendChild(blackHV1);
-
-document.getElementById('0 5').appendChild(whiteHV);
-
-document.getElementById('1 5').appendChild(whiteHV1);
-//
-document.getElementById('0 6').appendChild(blackT);
-
-document.getElementById('1 6').appendChild(blackT1);
-
-document.getElementById('0 7').appendChild(whiteT);
-
-document.getElementById('1 7').appendChild(whiteT1);
-//
-document.getElementById('0 8').appendChild(blackD);
-
-document.getElementById('1 8').appendChild(blackD1);
-
-document.getElementById('0 9').appendChild(whiteD);
-
-document.getElementById('1 9').appendChild(whiteD1);
-//
-document.getElementById('6 4').appendChild(whiteF);
-
-document.getElementById('7 5').appendChild(blackF);
-//
-document.getElementById('7 1').appendChild(blackJ);
-
-document.getElementById('8 1').appendChild(blackJ1);
-
-document.getElementById('7 2').appendChild(whiteJ);
-
-document.getElementById('8 2').appendChild(whiteJ1);
-
-document.getElementById('7 7').appendChild(blackJ2);
-
-document.getElementById('8 7').appendChild(blackJ3);
-
-document.getElementById('7 8').appendChild(whiteJ2);
-
-document.getElementById('8 8').appendChild(whiteJ3);
--- Variables.js
+++ Variables.js
@@ -11,100 +11,10 @@ var BoardArr = [
     [, , , , ]
 ];
 
-var htmlBoard = document.getElementById("htmlTable");
+var groundBoard = document.getElementById("groundTable");
+var skyBoard = document.getElementById("skyTable");
 
 var whiteAA = document.createElement("IMG");
 whiteAA.src = "Images/White/WhiteAA.png";
-
-var whiteAA1 = document.createElement("IMG");
-whiteAA1.src = "Images/White/WhiteAA.png";
-
-var blackAA = document.createElement("IMG");
-blackAA.src = "Images/Black/BlackAA.png";
-
-var blackAA1 = document.createElement("IMG");
-blackAA1.src = "Images/Black/BlackAA.png";
-//--
-var whiteS = document.createElement("IMG");
-whiteS.src = "Images/White/WhiteS.png";
-
-var whiteS1 = document.createElement("IMG");
-whiteS1.src = "Images/White/WhiteS.png";
-
-var blackS = document.createElement("IMG");
-blackS.src = "Images/Black/BlackS.png";
-
-var blackS1 = document.createElement("IMG");
-blackS1.src = "Images/Black/BlackS.png";
-//--
-var blackHV = document.createElement("IMG");
-blackHV.src = "Images/Black/BlackHV.png";
-
-var blackHV1 = document.createElement("IMG");
-blackHV1.src = "Images/Black/BlackHV.png";
-
-var whiteHV = document.createElement("IMG");
-whiteHV.src = "Images/White/WhiteHV.png";
-
-var whiteHV1 = document.createElement("IMG");
-whiteHV1.src = "Images/White/WhiteHV.png";
-//--
-
-var blackT = document.createElement("IMG");
-blackT.src = "Images/Black/BlackT.png";
-
-var blackT1 = document.createElement("IMG");
-blackT1.src = "Images/Black/BlackT.png";
-
-var whiteT = document.createElement("IMG");
-whiteT.src = "Images/White/WhiteT.png";
-
-var whiteT1 = document.createElement("IMG");
-whiteT1.src = "Images/White/WhiteT.png";
-//--
-
-var blackD = document.createElement("IMG");
-blackD.src = "Images/Black/BlackD.png";
-
-var blackD1 = document.createElement("IMG");
-blackD1.src = "Images/Black/BlackD.png";
-
-var whiteD = document.createElement("IMG");
-whiteD.src = "Images/White/WhiteD.png";
-
-var whiteD1 = document.createElement("IMG");
-whiteD1.src = "Images/White/WhiteD.png";
-//--
-
-var blackF = document.createElement("IMG");
-blackF.src = "Images/Black/BlackF.png";
-
-var whiteF = document.createElement("IMG");
-whiteF.src = "Images/White/WhiteF.png";
-//
-var blackJ = document.createElement("IMG");
-blackJ.src = "Images/Black/BlackJ.png";
-
-var blackJ1 = document.createElement("IMG");
-blackJ1.src = "Images/Black/BlackJ.png";
-
-var whiteJ = document.createElement("IMG");
-whiteJ.src = "Images/White/WhiteJ.png";
-
-var whiteJ1 = document.createElement("IMG");
-whiteJ1.src = "Images/White/WhiteJ.png";
-
-
-var blackJ2 = document.createElement("IMG");
-blackJ2.src = "Images/Black/BlackJ.png";
-
-var blackJ3 = document.createElement("IMG");
-blackJ3.src = "Images/Black/BlackJ.png";
-
-var whiteJ2 = document.createElement("IMG");
-whiteJ2.src = "Images/White/WhiteJ.png";
-
-var whiteJ3 = document.createElement("IMG");
-whiteJ3.src = "Images/White/WhiteJ.png";
 //
 
